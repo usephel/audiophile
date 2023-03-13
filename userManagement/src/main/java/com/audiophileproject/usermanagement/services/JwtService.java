70a130adce03c3d842ea07823ddb7599a01fa310
mehdi ouledhali <mehdi.ouledhali@gmail.com>
2023-03-13 01:33:16 +0100

    remove unused code

--- userManagement/src/main/java/com/audiophileproject/usermanagement/services/JwtService.java
+++ userManagement/src/main/java/com/audiophileproject/usermanagement/services/JwtService.java
@@ -82,11 +82,6 @@ public class JwtService {
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-//    private Key getSignInKey() {
-//        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
-//        return Keys.hmacShaKeyFor(keyBytes);
-//    }
-
 
 
 }
